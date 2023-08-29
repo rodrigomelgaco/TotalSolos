6bcd1d6d07e9c2cbaa838b66851de69d725033dc
Rodrigo Melgaço <melgacorodrigo@gmail.com>
2023-08-29 16:55:14 -0300

    Init

---
+++ src/main/java/br/com/melgaco/TotalSolosApplication.java
@@ -0,0 +1,13 @@
+package br.com.melgaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotalSolosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotalSolosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/melgaco/TotalSolosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.melgaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotalSolosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
